7f65e6860963b4764f296881fb02fe25eff3a59b
root <root@LAPTOP-MS0R01B3>
2023-10-06 13:10:23 +0530

    parameters working

--- install_fabric.sh
+++ install_fabric.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-fabric_resource_name=$1
-echo $fabric_resource_name
+# fabric_resource=$1
+# echo $fabric_resource
 
-curl -X POST https://api.fabric.microsoft.com/v1/workspaces/6bd1de78-9423-40f6-bf50-d9c146a39176/items \
--H 'Content-Type: application/json' \
--H 'Authorization: Bearer '${access_token} \
--d '{"displayName": "'${FABRIC_RESOURCE_NAME}'" ,"type": "Lakehouse" }' 
+fabric_resource_name=$1
+fabric_resource_type=$2
+echo $fabric_resource_name
+echo $fabric_resource_type
+
+# curl -X POST https://api.fabric.microsoft.com/v1/workspaces/6bd1de78-9423-40f6-bf50-d9c146a39176/items \
+# -H 'Content-Type: application/json' \
+# -H 'Authorization: Bearer '${access_token} \
+# -d '{"displayName": "'${FABRIC_RESOURCE_NAME}'" ,"type": "Lakehouse" }' 
 
