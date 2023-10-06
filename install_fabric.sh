b951929c015b4d788f02349ecc810667b3f44bb4
Guru Charan Bulusu <gurucb@microsoft.com>
2023-10-06 10:31:44 +0530

    dummy commit

--- install_fabric.sh
+++ install_fabric.sh
@@ -3,7 +3,6 @@
 fabric_resource_name=$1
 echo $fabric_resource_name
 
-
 curl -X POST https://api.fabric.microsoft.com/v1/workspaces/6bd1de78-9423-40f6-bf50-d9c146a39176/items \
 -H 'Content-Type: application/json' \
 -H 'Authorization: Bearer '${access_token} \
