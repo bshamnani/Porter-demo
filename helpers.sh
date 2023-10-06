10082447bdd16c07cd90922108f4a916326726c6
Guru Charan Bulusu <gurucb@microsoft.com>
2023-10-06 10:29:44 +0530

    Modified helper and install_fabric.sh

--- helpers.sh
+++ helpers.sh
@@ -2,13 +2,10 @@
 set -euo pipefail
 
 install() {
-  echo "Hello World bhavesh 123"
-  echo "$FABRIC_RESOURCE_NAME"
   access_token=$(/etc/getToken.sh|jq -r '.access_token')
-  curl -X POST https://api.fabric.microsoft.com/v1/workspaces/6bd1de78-9423-40f6-bf50-d9c146a39176/items \
-  -H 'Content-Type: application/json' \
-  -H 'Authorization: Bearer '${access_token} \
-  -d '{"displayName": "'${FABRIC_RESOURCE_NAME}'" ,"type": "Lakehouse" }' 
+  fabric_resource_name=$1
+  ./install_fabric.sh $access_token $fabric_resource_name
+  
 }
 
 upgrade() {
---
+++ install_fabric.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fabric_resource_name=$1
+echo $fabric_resource_name
+
+
+curl -X POST https://api.fabric.microsoft.com/v1/workspaces/6bd1de78-9423-40f6-bf50-d9c146a39176/items \
+-H 'Content-Type: application/json' \
+-H 'Authorization: Bearer '${access_token} \
+-d '{"displayName": "'${FABRIC_RESOURCE_NAME}'" ,"type": "Lakehouse" }' 
+
